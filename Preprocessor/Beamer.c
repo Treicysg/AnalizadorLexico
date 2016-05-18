fb1de88eced00e752a0fc053b88e3f13fdd676de
Denisse <denissepaolarojas@hotmail.com>
2016-05-18 17:44:56 -0600

    Primera presentacion Beamer

--- Preprocessor/Beamer.c
+++ Preprocessor/Beamer.c
@@ -3,9 +3,26 @@
 #include <stdlib.h>
 
 
-int main( int argc, char *argv[] )
+void createBeamer()
 {
   FILE* beamerFile = fopen( "beamerFile.tex", "w+" );
+  fprintf(beamerFile,"\\documentclass{beamer} \n");
+  fprintf(beamerFile,"\\title{A Tiny Example} \n");
+  fprintf(beamerFile,"\\date{\\today}\n");
+  fprintf(beamerFile,"\\begin{document} \n");
+  fprintf(beamerFile,"\\maketitle \n");
 
-  fprintf(beamerFile,"\\documentclass{beamer} newline",);
+  fprintf(beamerFile,"\\begin{frame} \n");
+  fprintf(beamerFile,"\\frametitle{First Slide} \n");
+  fprintf(beamerFile,"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \n");
+  fprintf(beamerFile,"\\end{frame} \n");
+  fprintf(beamerFile,"\\begin{frame} \n");
+  fprintf(beamerFile,"\\frametitle{Second Slide} \n");
+  fprintf(beamerFile,"Contents of the second slide \n");
+  fprintf(beamerFile,"\\end{frame} \n");
+  fprintf(beamerFile,"\\end{document}");
+
+  fclose(beamerFile);
+	system("pdflatex beamerFile.tex");
+	system("evince beamerFile.pdf");
 }
