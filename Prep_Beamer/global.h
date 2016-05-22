f8759ed3b338108d5d355d5e3f907ecd6d14fb75
Treicysg <treicysanchez.27@gmail.com>
2016-05-21 18:08:33 -0600

    Otra

--- Prep_Beamer/global.h
+++ Prep_Beamer/global.h
@@ -41,4 +41,4 @@ void count_tokens(int pToken);
 bool isInserted(int pTokenType);
 void insertTokenType(int pTokenType);
 void checkRegisteredTokens();
-const char * getTokenName(int tkn_type);
+void getTokenName(int tkn_type, char * to_filled);
